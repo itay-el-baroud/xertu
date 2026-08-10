04916393e05ac54789eb5f82ee1142819aa4cf10
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-10 20:17:22 +0300

    Format onCreate method for better readability

--- app/src/main/java/com/callshield/CallShieldApp.java
+++ app/src/main/java/com/callshield/CallShieldApp.java
@@ -1,8 +1,8 @@
 package com.callshield;
 import android.app.Application;
 public class CallShieldApp extends Application {
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
         super.onCreate();
-        try { com.callshield.utils.NotificationHelper.createChannel(this); } catch (Exception e) {}
     }
 }
