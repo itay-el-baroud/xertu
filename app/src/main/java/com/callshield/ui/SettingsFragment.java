e2274cfce0eac91c3cc265d961f0df0bc89e0d0b
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-10 20:23:40 +0300

    Simplify SettingsFragment to show a TextView
    Refactored onCreateView method to display a TextView instead of a settings layout.

--- app/src/main/java/com/callshield/ui/SettingsFragment.java
+++ app/src/main/java/com/callshield/ui/SettingsFragment.java
@@ -1,28 +1,7 @@
 package com.callshield.ui;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Switch;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.fragment.app.Fragment;
-import com.callshield.R;
-
+import android.os.Bundle; import android.view.*; import android.widget.TextView; import androidx.annotation.*; import androidx.fragment.app.Fragment;
 public class SettingsFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_settings, container, false);
-        Switch nightSwitch = v.findViewById(R.id.nightSwitch);
-        int mode = AppCompatDelegate.getDefaultNightMode();
-        nightSwitch.setChecked(mode == AppCompatDelegate.MODE_NIGHT_YES);
-        nightSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        });
-        return v;
+    @Nullable @Override public View onCreateView(@NonNull LayoutInflater i, @Nullable ViewGroup c, @Nullable Bundle b){
+        TextView tv=new TextView(getContext()); tv.setText("Settings"); return tv;
     }
 }
