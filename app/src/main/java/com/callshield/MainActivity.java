4931887bcaeea5e8fd8a94de81fa92e1e3f934ea
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-10 18:58:27 +0300

    Refactor onCreate for better login handling
    Refactor onCreate method to handle exceptions and improve login flow.

--- app/src/main/java/com/callshield/MainActivity.java
+++ app/src/main/java/com/callshield/MainActivity.java
@@ -1,20 +1,24 @@
 package com.callshield;
-
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.callshield.data.TokenManager;
-
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TokenManager tokenManager = new TokenManager(this);
-        if (tokenManager.isLoggedIn()) {
-            startActivity(new Intent(this, HomeActivity.class));
-        } else {
+        try {
+            setContentView(R.layout.activity_main);
+            TokenManager tm = new TokenManager(this);
+            if (tm.isLoggedIn()) {
+                startActivity(new Intent(this, HomeActivity.class));
+            } else {
+                startActivity(new Intent(this, LoginActivity.class));
+            }
+            finish();
+        } catch (Exception e) {
+            e.printStackTrace();
             startActivity(new Intent(this, LoginActivity.class));
+            finish();
         }
-        finish();
     }
 }
