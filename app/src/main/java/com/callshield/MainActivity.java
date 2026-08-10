2900df8960935b10123a5797e16b3b5acfc7ff01
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-10 20:38:19 +0300

    Update MainActivity to show a TextView message
    Replaced the existing content view with a TextView displaying a message in Arabic.

--- app/src/main/java/com/callshield/MainActivity.java
+++ app/src/main/java/com/callshield/MainActivity.java
@@ -1,24 +1,17 @@
 package com.callshield;
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import com.callshield.data.TokenManager;
 public class MainActivity extends AppCompatActivity {
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            setContentView(R.layout.activity_main);
-            TokenManager tm = new TokenManager(this);
-            if (tm.isLoggedIn()) {
-                startActivity(new Intent(this, HomeActivity.class));
-            } else {
-                startActivity(new Intent(this, LoginActivity.class));
-            }
-            finish();
-        } catch (Exception e) {
-            e.printStackTrace();
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
-        }
+        try{
+            TextView tv = new TextView(this);
+            tv.setText("CallShield شغال - دوس رجوع وادخل Home");
+            tv.setTextSize(20);
+            tv.setPadding(40,300,40,40);
+            setContentView(tv);
+        }catch(Exception e){}
     }
 }
