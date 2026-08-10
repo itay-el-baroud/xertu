9675d7ae712d81c95a5499e04a1ed590811bc828
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-10 18:57:14 +0300

    Refactor CallShieldApp to simplify initialization
    Removed WorkManager configuration and updated NotificationHelper usage.

--- app/src/main/java/com/callshield/CallShieldApp.java
+++ app/src/main/java/com/callshield/CallShieldApp.java
@@ -1,18 +1,10 @@
 package com.callshield;
-
 import android.app.Application;
-import androidx.work.Configuration;
-import com.callshield.util.NotificationHelper;
-
-public class CallShieldApp extends Application implements Configuration.Provider {
-    @Override
-    public void onCreate() {
+public class CallShieldApp extends Application {
+    @Override public void onCreate() {
         super.onCreate();
-        NotificationHelper.createChannels(this);
-    }
-
-    @Override
-    public Configuration getWorkManagerConfiguration() {
-        return new Configuration.Builder().build();
+        try {
+            com.callshield.utils.NotificationHelper.createChannel(this);
+        } catch (Exception e) { e.printStackTrace(); }
     }
 }
